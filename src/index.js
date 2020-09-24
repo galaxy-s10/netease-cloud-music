e8fab661837a7752ddb6f350deb95b1ae6c3df44
Your Name <you@example.com>
2020-09-25 00:13:20 +0800

    feat: 去掉严格模式

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './App';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
