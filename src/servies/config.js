ac96dd750c86c5683e1e42875420d33e45abc5d6
shuisheng <2274751790@qq.com>
2023-04-08 04:02:59 +0800

    feat: api

--- src/servies/config.js
+++ src/servies/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://project.hsslive.cn:3100/";
+// export const BASE_URL = "http://project.hsslive.cn:3100/";
+export const BASE_URL = "https://api.hsslive.cn/netease-cloud-music-api/";
 
 export const TIMEOUT = 5000;
